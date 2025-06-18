b24f2d821e890ff71b22d4ae1692754a3ea3b256
Riad <119328612+riadghub@users.noreply.github.com>
2025-06-18 11:46:29 +0200

    Add files via upload

--- srcs/executor/executor_heredoc_pre.c
+++ srcs/executor/executor_heredoc_pre.c
@@ -6,7 +6,7 @@
 /*   By: reeer-aa <reeer-aa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 10:16:04 by reeer-aa          #+#    #+#             */
-/*   Updated: 2025/06/18 10:16:16 by reeer-aa         ###   ########.fr       */
+/*   Updated: 2025/06/18 11:42:16 by reeer-aa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,60 @@ static char	*generate_temp_filename(void)
 	return (filename);
 }
 
-static int	process_single_heredoc(t_redir *redir)
+static void	heredoc_signal_handler(int sig)
+{
+	(void)sig;
+	g_signal_status = 130;
+	printf("\r> ^C\n");
+	close(STDIN_FILENO);
+}
+
+static int	handle_heredoc_input(t_redir *redir, int fd, int original_stdin)
 {
 	char	*line;
+	int		saved_status;
+
+	saved_status = g_signal_status;
+	g_signal_status = 0;
+	signal(SIGINT, heredoc_signal_handler);
+	signal(SIGQUIT, SIG_IGN);
+	while (1)
+	{
+		line = readline("> ");
+		if (!line)
+		{
+			if (g_signal_status == 130)
+			{
+				dup2(original_stdin, STDIN_FILENO);
+				return (1);
+			}
+			break ;
+		}
+		if (g_signal_status == 130)
+		{
+			free(line);
+			dup2(original_stdin, STDIN_FILENO);
+			return (1);
+		}
+		if (ft_strcmp(line, redir->file) == 0)
+		{
+			free(line);
+			break ;
+		}
+		write(fd, line, ft_strlen(line));
+		write(fd, "\n", 1);
+		free(line);
+	}
+	g_signal_status = saved_status;
+	return (0);
+}
+
+static int	process_single_heredoc(t_redir *redir)
+{
 	char	*temp_file;
 	int		fd;
+	int		original_stdin;
+	int		result;
 
 	temp_file = generate_temp_filename();
 	if (!temp_file)
@@ -41,20 +90,23 @@ static int	process_single_heredoc(t_redir *redir)
 		free(temp_file);
 		return (1);
 	}
-	while (1)
+	original_stdin = dup(STDIN_FILENO);
+	if (original_stdin == -1)
 	{
-		line = readline("> ");
-		if (!line || ft_strcmp(line, redir->file) == 0)
-		{
-			if (line)
-				free(line);
-			break ;
-		}
-		write(fd, line, ft_strlen(line));
-		write(fd, "\n", 1);
-		free(line);
+		close(fd);
+		free(temp_file);
+		return (1);
 	}
+	result = handle_heredoc_input(redir, fd, original_stdin);
+	close(original_stdin);
 	close(fd);
+	setup_signals();
+	if (result == 1)
+	{
+		unlink(temp_file);
+		free(temp_file);
+		return (1);
+	}
 	free(redir->file);
 	redir->file = temp_file;
 	redir->type = TOKEN_REDIR_IN;
@@ -89,7 +141,7 @@ int	preprocess_heredocs_in_node(t_ast_node *node)
 
 void	cleanup_temp_files(t_ast_node *node)
 {
-	t_redir	*redir;
+	t_redir *redir;
 
 	if (!node)
 		return ;
